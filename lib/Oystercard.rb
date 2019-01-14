a62720376f639a9ceb31bfb328006b3b851b57e8
AdrienFabre <adrien.fabre.1@gmail.com>
2019-01-14 14:58:25 +0000

    5_Top up

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -6,4 +6,7 @@ def initialize
   @balance = 0
 end
 
+def top_up(amount)
+  @balance += amount
+end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,11 +2,26 @@ require "Oystercard"
 
 describe Oystercard do
 
-  it { is_expected.to respond_to(:balance) }
+  describe '#balance' do
 
-  it "should have a default new balance of £0" do
-    card = Oystercard.new
-    expect(card.balance).to eq 0
+    it { is_expected.to respond_to(:balance) }
+
+    it "should have a default new balance of £0" do
+      card = Oystercard.new
+      expect(card.balance).to eq 0
+    end
+
   end
 
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1) }
+
+    it "should be able to top up the balance" do
+      card = Oystercard.new
+      card.top_up(10)
+      expect(card.balance).to eq 10
+    end
+
+  end
 end
