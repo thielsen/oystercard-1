142e3223fc1795cc030cca9b6ca061ee102d6a07
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-14 14:31:15 +0000

    created Oystercard.rb and added the balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,4 +2,11 @@ require "Oystercard"
 
 describe Oystercard do
 
+  it { is_expected.to respond_to(:balance) }
+
+  it "should have a default new balance of £0" do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
+
 end
