e4307e0603f36df512e48572b87606e2d48a8961
Simon Green <simon.green@thielsen.co.uk>
2019-01-17 20:44:02 +0000

    Update journey_spec.rb

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -6,7 +6,16 @@ it {is_expected.to respond_to(:start)}
 it {is_expected.to respond_to(:finish)}
 it {is_expected.to respond_to(:journeys)}
 
-it ''
+it 'is expected to start a journey' do
+
+end
+it 'is expected to end a journey' do
+
+end
+it 'is expected to reyturn a list of journeys' do
+
+end
+
 
 
 
